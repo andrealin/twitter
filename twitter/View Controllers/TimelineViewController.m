1dd05a7eb3d01a60ac26e7e9a4bb31340a3cdac6
Andrea Lin <drealin@fb.com>
2019-07-03 11:39:10 -0700

    cleanup

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -35,12 +35,6 @@
         if (tweets) {
             self.tweets = tweets; // view controller stores data passed into the completion handler
             NSLog(@"😎😎😎 Successfully loaded home timeline");
-//            for (Tweet *dictionary in tweets) {
-//////                NSLog(@"loop");
-//                NSString *text = dictionary.text;
-//                NSLog(@"%@", text);
-////                NSLog(@"%@", dictionary);
-//            }
             [self.tableView reloadData]; // reload the table view. table view asks its dataSource for numberOfRows & cellForRowAt
         } else {
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
@@ -58,7 +52,6 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // numberOfRows returns the number of items returned from the API
-    NSLog(@"number of rows function called");
     return self.tweets.count;
 }
 
@@ -80,29 +73,7 @@
     
     NSURL *URL = [NSURL URLWithString:URLString];
     
-    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
-    
-    [cell.userImageView setImageWithURLRequest:request placeholderImage:nil
-                                    success:^(NSURLRequest *imageRequest, NSHTTPURLResponse *imageResponse, UIImage *image) {
-                                        
-                                        // imageResponse will be nil if the image is cached
-                                        if (imageResponse) {
-                                            NSLog(@"Image was NOT cached, fade in image");
-                                            cell.userImageView.alpha = 0.0;
-                                            cell.userImageView.image = image;
-                                            
-                                            //Animate UIImageView back to alpha 1 over 0.3sec
-                                            [UIView animateWithDuration:0.3 animations:^{
-                                                cell.userImageView.alpha = 1.0;
-                                            }];
-                                        }
-                                        else {
-                                            NSLog(@"Image was cached so just update the image");
-                                            cell.userImageView.image = image;
-                                        }
-                                    }
-                                    failure:nil];
-    
+    [cell.userImageView setImageWithURL:URL];
     
     return cell;
 }
